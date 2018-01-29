561f5bc0c5fd52126dcb7fa89e8f1ff9514b9928
Lyuben Branzalov <Leny73@users.noreply.github.com>
2018-01-29 15:15:39 +0000

    Add files via upload
    feature: Creating the functionality of the project The function makeGrid which creates a grid, an eventListener on the button, and an  event listener on the cells of the grid to give them specific colour

--- designs.js
+++ designs.js
@@ -1,10 +1,24 @@
-// Select color input
-// Select size input
-
 // When size is submitted by the user, call makeGrid()
-
+$("#button").on("click",function(event){
+event.preventDefault();
+makeGrid();
+});
+// Function that creates the grid
 function makeGrid() {
-
-// Your code goes here!
-
+const width = $("#input_width").val();
+const height = $("#input_height").val();
+let grid = $("#pixel_canvas");
+grid.children().remove();
+for(var r = 0 ; r<height; r++){
+    grid.append('<tr class ="tr"></tr>');
+    for(var c = 0; c<width; c++){
+        grid.children().last().append('<td class="td"> </td>');
+    }
+}
+//Event listener that colors the cells with specific color
+const td = $(".td");
+td.on("click", function() {
+    const color = $("#colorPicker").val();
+    $(this).css("background-color",color);
+});
 }
